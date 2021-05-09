3931a8e736c1d8c4414baa89589e10cb4fb1fa26
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-05-09 16:41:10 -0400

    Moved hardcoded variables to single class

---
+++ code/tp3/src/main/java/com/tp2/utils/EnvironmentVariables.java
@@ -0,0 +1,21 @@
+package com.tp2.utils;
+
+public class EnvironmentVariables {
+
+    public static final String HOST_URL = "http://ec2-35-182-16-246.ca-central-1.compute.amazonaws.com:4200";
+
+    // QR code
+    public static final String QR_URL = HOST_URL + "/permis/verify/";
+    public static final int QR_WIDTH = 300;
+    public static final int QR_HEIGHT = 300;
+    public static final String QR_FORMAT = "PNG";
+    public static final String QR_MIME_TYPE = "image/png";
+
+
+    // Emails
+    public static final String EMAIL_SUBJECT = "QR code covid";
+    public static final String EMAIL_BODY = "Voici votre code qr dans le format choisit";
+    public static final String EMAIL_PDF_MIME_TYPE = "application/pdf";
+
+
+}
